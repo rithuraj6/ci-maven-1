8c202af9d4140af1bdb704283507884ec7b7acbf
rithu <contactrithuraj@gmail.com>
2026-03-16 15:46:37 +0530

    initjalcommit

---
+++ src/test/java/com/demo/AppTest.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    void testAdd() {
+        assertEquals(5, App.add(2, 3));
+    }
+
+}
